700df58f72e88fa0cb91481bdf7706c133255df5
hiikalll <ekal.ehmm@gmail.com>
2023-02-14 16:22:59 +0700

    add seeders

---
+++ database/seeders/CampTableSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Camp;
+
+class CampTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $camps = [
+            [
+                'title' => 'Laravel Mastery',
+                'slug' => 'laravel-mastery',
+                'price' => 280,
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+            [
+                'title' => 'Laravel Jr',
+                'slug' => 'laravel-jr',
+                'price' => 140,
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+        ];
+
+        foreach ($camps as $key => $camp) {
+            Camp::create($camp);
+        }
+
+        // 2nd methode
+        // Camp::insert($camps);
+    }
+}
